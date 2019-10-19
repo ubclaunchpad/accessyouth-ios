4234afa568362a4b381a1cd95d87136fa8d548b4
Schemetrical <Schemetrical@icloud.com>
2019-10-19 15:56:10 -0700

    Creates empty project

---
+++ AccessYouth/AccessYouth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessYouth
+//
+//  Created by Yichen Cao on 2019-10-19.
+//  Copyright © 2019 UBC Launch Pad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AccessYouth/AccessYouthOperator/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  AccessYouthOperator
+//
+//  Created by Yichen Cao on 2019-10-19.
+//  Copyright © 2019 UBC Launch Pad. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = UIViewController()
+        homeViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+        return true
+    }
+
+
+}
+
